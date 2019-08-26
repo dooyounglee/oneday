5ddb0fc607cfe3397ff6ab143bdc3250bf4f9096
dooyounglee <gostbaducking1@nate.com>
2019-08-26 23:38:34 +0900

    Update MemberService.java

--- src/main/java/com/lj/service/MemberService.java
+++ src/main/java/com/lj/service/MemberService.java
@@ -2,104 +2,34 @@ package com.lj.service;
 
 import java.util.ArrayList;
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Service;
-import org.springframework.transaction.annotation.Transactional;
-
-import com.lj.dao.MemberDAO;
 import com.lj.vo.ClassVO;
 import com.lj.vo.JoinAuth;
 import com.lj.vo.MemberVO;
 
-import lombok.AllArgsConstructor;
-import lombok.Setter;
-import lombok.extern.log4j.Log4j;
+public interface MemberService {
 
-@Log4j
-@Service
-@AllArgsConstructor
-public class MemberServiceImpl implements MemberService {
-
-	@Setter(onMethod_ = @Autowired)
-	private MemberDAO md;
+	public int join(MemberVO m);
 	
-	@Override
-	public int join(MemberVO m) {
-		int result=0;
-		result=md.join(m);
-		return result;
-	}
+	public int joinH(MemberVO m);
+	
+	public MemberVO getMember(String email,String pass);
 
-	@Transactional
-	@Override
-	public int joinH(MemberVO m) {
-		int result=0;
-		m.setHost("Y");
-		System.out.println(md.join(m));
-		
-		MemberVO host=getMember(m.getEmail(),m.getPass());
-		host.setPhone(m.getPhone());
-		result=md.joinH(host);
-		return result;
-	}
+	public ArrayList<MemberVO> getMemberList();
+	
+	public ArrayList<MemberVO> getHostList();
 
-	@Override
-	public MemberVO getMember(String email, String pass) {
-		MemberVO mem=new MemberVO();
-		mem.setEmail(email);
-		mem.setPass(pass);
-		MemberVO m=md.getMember(mem);
-		return m;
-	}
+	public ArrayList<MemberVO> getAllList();
 
-	@Override
-	public ArrayList<MemberVO> getMemberList() {
-		return md.getMemberList();
-	}
+	public int leave(MemberVO m);
 
-	@Override
-	public ArrayList<MemberVO> getHostList() {
-		return md.getHostList();
-	}
+	public ArrayList<ClassVO> getMyClassList(MemberVO mem);
 
-	@Override
-	public ArrayList<MemberVO> getAllList() {
-		return md.getAllList();
-	}
+	public void insertAuth(JoinAuth joinauth);
 
-	@Override
-	public int leave(MemberVO m) {
-		int result=0;
-		result=md.leave(m);
-		return result;
-	}
+	public int auth(JoinAuth joinauth);
 
-	@Override
-	public ArrayList<ClassVO> getMyClassList(MemberVO mem) {
-		return md.getMyClassList(mem);
-	}
+	public int isAuth(MemberVO m);
 
-	@Override
-	public void insertAuth(JoinAuth joinauth) {
-		md.insertAuth(joinauth);
-	}
-
-	@Override
-	public int auth(JoinAuth joinauth) {
-		int result=md.auth(joinauth);
-		return result;
-	}
-
-	@Override
-	public int isAuth(MemberVO m) {
-		int result=md.isAuth(m);
-		return result;
-	}
-
-	@Override
-	public int changePass(MemberVO mem) {
-		int result=md.changePass(mem);
-		return result;
-	}
+	public int changePass(MemberVO mem);
 	
 }
