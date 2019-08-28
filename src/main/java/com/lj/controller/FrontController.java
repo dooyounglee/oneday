c8390c3106d96531f34e4a4d6fb44cb5392daedd
dooyounglee <gostbaducking1@nate.com>
2019-08-28 11:21:03 +0900

    Update FrontController.java

--- src/main/java/com/lj/controller/FrontController.java
+++ src/main/java/com/lj/controller/FrontController.java
@@ -1,17 +1,24 @@
 package com.lj.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
-import com.fasterxml.jackson.annotation.JacksonInject.Value;
+import com.lj.service.ClassService;
+
+import lombok.Setter;
 
 @Controller
 @RequestMapping("/*")
 public class FrontController {
 
+	@Setter(onMethod_=@Autowired)
+	private ClassService cService;
+	
 	// 메인화면
 	@RequestMapping(value= "/")
-	public String index(){
+	public String index(Model model){
 		model.addAttribute("clist", cService.getList());
 		return "index";
 	}
