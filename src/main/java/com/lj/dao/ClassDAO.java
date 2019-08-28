53181d8c589a47561068ad8f8d9fa03f3028291e
dooyounglee <gostbaducking1@nate.com>
2019-08-28 09:32:01 +0900

    Update ClassDAO.java

--- src/main/java/com/lj/dao/ClassDAO.java
+++ src/main/java/com/lj/dao/ClassDAO.java
@@ -1,6 +1,19 @@
 package com.lj.dao;
 
+import java.util.List;
+
+import com.lj.vo.ClassVO;
+
 public interface ClassDAO {
 
+	public void addClass(ClassVO vo);
+	
+	public List<ClassVO> getList();
+	
+	public ClassVO get(int cno);
+	
+	public List<ClassVO> getMyList(int mno);
+	
+	public int updateClass(ClassVO vo);
 	
 }
